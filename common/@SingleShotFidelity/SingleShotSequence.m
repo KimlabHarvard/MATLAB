4c94366b300d0fa4d6f15fb01e62c270c998ce31
Crossno <JDCrossno@gmail.com>
2016-06-25 15:00:05 -0400

    stripped
    stripped out many functions not needed at Harvard

--- common/@SingleShotFidelity/SingleShotSequence.m
+++
@@ -1,7 +0,0 @@
-function SingleShotSequence(obj, qubit)
-
-[thisPath, ~] = fileparts(mfilename('fullpath'));
-scriptName = fullfile(thisPath, 'SingleShotSequence.py');
-[status, result] = system(sprintf('python "%s" "%s" %s', scriptName, getpref('qlab', 'PyQLabDir'), qubit), '-echo');
-
-end
